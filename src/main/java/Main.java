f0c098a1edc4019f4d6d4742b15e2ecb6adfb027
Иван Илляшенко <illyashenko@inbox.ru>
2021-04-16 15:00:03 +0300

    #add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 import static spark.Spark.*;
 
 public class Main {
+
+    private static final int CODE_OK = 200;
+
     public static void main(String[] args) {
         port(5002);
         post("/api", (request, response) -> {
             var stringResponse = Service.GetDataFromSql(request.body());
-            response.status(200);
+            response.status(CODE_OK);
             response.type("application/json");
             return stringResponse;
         });
+        get("/about", ((request, response) -> {
+            response.status(CODE_OK);
+            return "it's work";
+        }));
     }
 }
